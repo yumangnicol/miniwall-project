eb4e8a10786b543d5c362b6d283e562d54dcf3a7
Nicol Luis Yumang <nicolluisyumang@nicols-air.home>
2022-10-23 22:07:34 +0100

    used mongoose to connect to the db in app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+mongoose.connect(process.env.DB_CONNECTOR, ()=>{
+    console.log('DB is connected...')
+})
+
 app.listen(3000, ()=>{
     console.log('Your server is running...')
 })
