8a04efd25758e156caead00b96b34b8d1e23bb4e
nicolluisyumang <yumangnicol@gmail.com>
2022-11-12 20:57:19 +0000

    Added delete post route

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,6 @@ const Post = require('../models/Post')
 const {postValidation} = require('../validations/validation')
 const verifyToken = require('../verifyToken')
 
-
 // Creates new Post
 router.post('/', verifyToken, async(req, res)=>{
 
@@ -15,13 +14,14 @@ router.post('/', verifyToken, async(req, res)=>{
         return res.status(400).send({message:error['details'][0]['message']})
     }
 
-    // Saves Post data
+    // Create new Post
     const postData = new Post({        
         user_id: res.user._id,
         title: req.body.title,
         description: req.body.description        
     })    
 
+    // Saves new Post
     try {
         const postToSave = await postData.save()
         res.send(postToSave)
@@ -32,7 +32,6 @@ router.post('/', verifyToken, async(req, res)=>{
 
 // Reads all Posts, arranged in descending order by likes_count
 router.get('/', verifyToken, async(req, res)=>{
-
     try {
         const getPosts = await Post.find().sort({likes_count:-1})
         res.send(getPosts)        
@@ -43,14 +42,14 @@ router.get('/', verifyToken, async(req, res)=>{
 
 // Read Post
 router.get('/:postId', verifyToken, async(req,res)=>{
-    try {
-        const getPostById = await Post.findById(req.params.postId)
+    const getPostById = await Post.findById(req.params.postId)
 
-        // Validation 1: Sends error message if post is not found
-        if(getPostById == null){
-            return res.status(400).send({message:"Cannot find post with that id"})
-        }
+    // Validation 1: Sends error message if post is not found
+    if(getPostById == null){
+        return res.status(400).send({message:"Cannot find post with that id"})
+    }
 
+    try {                
         res.send(getPostById)
     } catch (error) {
         res.send({message: error})
@@ -59,30 +58,57 @@ router.get('/:postId', verifyToken, async(req,res)=>{
 
 // Update Post
 router.patch('/:postId', verifyToken, async(req,res)=>{
-    // Validation 1: Check if User is owner of Post
-    const ownPost = await Post.findOne({"_id" :req.params.postId, user_id: res.user._id})
-    if(!ownPost){
+    const getPostById = await Post.findById(req.params.postId)     
+
+    // Validation 1: Sends error message if post is not found
+    if(getPostById == null){
+        return res.status(400).send({message:"Cannot find post with that id"})
+    }
+   
+    // Validation 2: Check if User is owner of Post
+    if(getPostById.user_id != res.user._id) {
         return res.status(400).send({message:"User is not allowed to update this post"})
     }
 
-    // Validation 2: Checks User input
+    // Validation 3: Checks User input
     const {error} = postValidation(req.body)
     if(error){
         return res.status(400).send({message:error['details'][0]['message']})
     }
 
     // Saves changes to Post data
-    try {
-        const updatePostById = await Post.updateOne(
-            {_id:req.params.postId},
+    try {   
+        const updatePostById = await getPostById.updateOne(
             {$set: {
                 title: req.body.title,
                 description: req.body.description  
             }}
-        )   
+        )               
         res.send(updatePostById)
     } catch (error) {
         res.status(400).send({message:error})
+    }
+})
+
+// Delete Post
+router.delete('/:postId', verifyToken, async(req,res)=>{
+    const getPostById = await Post.findById(req.params.postId)   
+
+    // Validation 1: Sends error message if post is not found
+    if(getPostById == null){
+        return res.status(400).send({message:"Cannot find post with that id"})
+    }
+
+    // Validation 2: Check if User is owner of Post   
+    if(getPostById.user_id != res.user._id){
+        return res.status(400).send({message:"User is not allowed to delete this post"})
+    }
+    
+    try {
+        const deletePostById = await getPostById.deleteOne()
+        res.send(deletePostById)
+    } catch (error) {
+        res.send({message:error})
     }
 })
 
