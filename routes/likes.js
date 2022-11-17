723c95179d8172379d72ca2c8d9f46f8f019dc1c
nicolluisyumang <yumangnicol@gmail.com>
2022-11-17 17:16:11 +0000

    Refactor likes.js route

--- routes/likes.js
+++ routes/likes.js
@@ -2,40 +2,35 @@ const express = require('express')
 const router = express.Router({mergeParams:true})
 
 const Post = require('../models/Post')
+const verifyToken = require('../middleware/verifyToken')
 
-router.post('/', async(req,res)=>{
+router.post('/', verifyToken, async(req,res)=>{    
+    const getPostById = await Post.findOne({"_id" :req.params.postId})
 
-    // Validation 1: Check if User is owner of Post
-    const ownPost = await Post.findOne({"_id" :req.params.postId, user_id: res.user._id})
-    if(ownPost){
+    // Validation 1: Sends error message if post is not found
+    if(getPostById == null){
+        return res.status(400).send({message:"Cannot find post with that id"})        
+    }
+
+    // Validation 2: Checks if User is owner of Post    
+    if(getPostById.user_id == res.user._id){
         return res.status(400).send({message:"User cannot like own post"})
     }
 
-    // Validation 2: Check if User has already liked Post
-    try {
-        const hasLiked = await Post.findOne({ "_id" :req.params.postId, "likes.user_id": res.user._id})    
-        if(hasLiked){
-            return res.status(400).send({message:"User has already liked post"})
-        } 
-    } catch (error) {
-        res.status(400).send({message:error})
-    }    
+    // Validation 3: Checks if User has already liked Post
+    const hasLiked = await Post.findOne({ "_id" :req.params.postId, "likes.user_id": res.user._id})    
+    if(hasLiked){
+        return res.status(400).send({message:"User has already liked post"})
+    }  
 
     // Saves a new Like to a Post and increments likes_count by 1
-    try {
-        const pushLikeToPost = await Post.findById(req.params.postId)
-        
-        // Validation 3: Sends error message if post is not found
-        if(pushLikeToPost == null){
-            return res.status(400).send({message:"Cannot find post with that id"})
-        }          
-
-        pushLikeToPost.likes.push({
+    try {        
+        getPostById.likes.push({
             user_id: res.user._id
         })
-        pushLikeToPost.$inc('likes_count', 1)        
-        const savedLike = await pushLikeToPost.save()
-        res.send(pushLikeToPost)
+        getPostById.$inc('likes_count', 1)        
+        const savedLike = await getPostById.save()
+        res.send(savedLike)
     } catch (error) {
         res.status(400).send({message:error})
     }
