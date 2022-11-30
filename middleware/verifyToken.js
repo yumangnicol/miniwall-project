4d866188563b2230e1913f70949861413a659f22
nicolluisyumang <yumangnicol@gmail.com>
2022-11-30 15:23:50 +0000

    Added comments to verifytoken.js

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -7,8 +7,11 @@ function auth(req,res,next){
     }
 
     try {
+        // Verifies access_token passed on the request header
         const verified = jsonwebtoken.verify(token,process.env.TOKEN_SECRET)
         req.user = verified
+
+        // Continues to main request
         next()
     } catch (error) {
         return res.status(401).send({message:'Invalid token'})
